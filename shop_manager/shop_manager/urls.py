d492ec0f1c8594c6fb110f6c0e00437049db0e4c
JAiga1 <150318215+JAiga1@users.noreply.github.com>
2025-03-05 22:51:07 +0530

    Update urls.py

--- shop_manager/shop_manager/urls.py
+++ shop_manager/shop_manager/urls.py
@@ -1,14 +1,14 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Home Page")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('', home),  # Default homepage
 ]
 
-# ✅ Serve media files in development mode
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
